cac262d5d985b5e788dd327797e1aed7ebfebce0
李明书 <547105745@qq.com>
2018-08-21 00:41:30 +0800

    0821

--- model.py
+++ model.py
@@ -5,16 +5,19 @@ db = SQLAlchemy(use_native_unicode='utf8')
 
 
 class Good(db.Model):
-    good_id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'good'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     good_pic_url = db.Column(db.String(20), nullable=False)
     good_price = db.Column(db.Integer, nullable=False)
     good_name = db.Column(db.String(20), nullable=False)
 
 
 
-class All(db.Model):
-    all_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(20), nullable=False)
-    good_num = db.Column(db.Integer, nullable=False)
-    good_id = db.Column(db.Integer, nullable=False)
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(20), nullable=False, unique=True)
+    num = db.Column(db.Integer, nullable=False)
+    good_id = db.Column(db.Integer, db.ForeignKey('good.id'))
     time = db.Column(db.Date, default=datetime.date.today())
+    good_info = db.relationship('Good', backref=db.backref('orders'))
